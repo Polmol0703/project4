9f7043377a7674a3149c1e91cfc395b46ca041d7
saikannan-sampath <saikannan_sampath@berkeley.edu>
2018-05-08 15:20:11 -0700

    task3b

--- Tas1script.m
+++ Tas1script.m
@@ -1,12 +1,12 @@
 rho = 1.18; %kg/m3
-v_1 = 12; %m/s
+v_1 = 6.5; %m/s
 alpha = 8; %deg
 c_l = 1.27; 
 n = 3; % numb of blades
-k_h = 2.7; %metres
+k_h = 2.975; %metres
 sigma = 0.3;
-r_h = 3; %metres
+r_h = 3.5; %metres
 R = 35; %metres
-w = 2; %rad/s
+w = 4; %rad/s
 
 [power, betz_power,betz_eff] = Task1(rho,v_1, alpha, c_l,n,k_h, sigma,r_h, R, w) 
--- Task3.m
+++ Task3.m
@@ -8,14 +8,21 @@ n = 3;
 omega = 4;
 counter1 = 1;
 
-for R = 20:0.5:100 %looping over values of R
+for R = 1:0.05:50 %looping over values of R
     K_h = 0.085*R;
     r_h = 0.1*R;
     counter2 = 1;
-    for sigma = 0:0.01:1 %looping over values of sigma
+    for sigma = 0:0.001:1 %looping over values of sigma
         [power(counter1,counter2), betz_power(counter1,counter2), betz_eff(counter1,counter2)] = Task1(rho ,v, alpha, C_l, n, K_h, sigma,r_h, R, omega);
         counter2 = counter2 + 1;
     end
     counter1 = counter1 + 1;
 end
 
+power2 = power(power <1500.1 & power >1499.9)
+
+error = abs(power2 - 1500);
+index = find(error == min(error))
+[row,col] = find(power == power2(index))
+Rfinal = 1 + row * 0.05
+sigmaFinal = 0 + col * 0.001
